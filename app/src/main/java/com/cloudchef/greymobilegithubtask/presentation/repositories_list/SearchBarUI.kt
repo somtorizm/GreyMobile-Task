13d3a0b705423a1af71c7827405c0ca12de040bc
“somtorizm” <42660335+somtorizm@users.noreply.github.com>
2024-07-06 17:42:19 +0100

    implement search repo layout

--- app/src/main/java/com/cloudchef/greymobilegithubtask/presentation/repositories_list/SearchBarUI.kt
+++ app/src/main/java/com/cloudchef/greymobilegithubtask/presentation/repositories_list/SearchBarUI.kt
@@ -1,73 +1,82 @@
 package com.cloudchef.greymobilegithubtask.presentation.repositories_list
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import com.cloudchef.greymobilegithubtask.R
+import com.cloudchef.greymobilegithubtask.ui.theme.ButtonColor
+import com.cloudchef.greymobilegithubtask.ui.theme.GreyMobileGithubTaskTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchBar() {
     var searchQuery by remember { mutableStateOf(TextFieldValue("")) }
 
-    Row(
-        modifier = Modifier
-            .border(
-                width = 1.dp,
-                color = Color.Gray,
-                shape = RoundedCornerShape(8.dp)
-            )
-            .fillMaxWidth()
-            .height(60.dp)
-    ) {
-        TextField(
-            value = searchQuery,
-            onValueChange = { searchQuery = it },
-            placeholder = { Text("Search for users...", color = MaterialTheme.colors.background) },
-            leadingIcon = {
-                Icon(
-                    imageVector = ImageVector.vectorResource(id = R.drawable.search),
-                    contentDescription = "Search Icon"
-                )
-            },
-            modifier = Modifier
-                .weight(1f)
-                .padding(start = 8.dp)
-                .border(width = 0.dp, color = Color.Transparent),
-            singleLine = true,
-            colors = TextFieldDefaults.textFieldColors(
-                backgroundColor = Color.Transparent,
-                focusedIndicatorColor = Color.Transparent,
-                unfocusedIndicatorColor = Color.Transparent
-            )
-        )
+    GreyMobileGithubTaskTheme {
 
-        Button(
-            onClick = { /* Handle search action */ },
+        Row(
             modifier = Modifier
-                .fillMaxHeight()
-                .clip(RoundedCornerShape(topEnd = 8.dp, bottomEnd = 8.dp))
-                .padding(10.dp),
-            colors = ButtonDefaults.buttonColors(
-                backgroundColor = colorResource(id = R.color.btn_color),
-                contentColor = Color.White 
-            )
+                .border(
+                    width = 1.dp,
+                    color = Color.Gray,
+                    shape = RoundedCornerShape(8.dp)
+                )
+                .fillMaxWidth()
+                .height(60.dp)
         ) {
-            Text(
-                "Search",
-                fontSize = 16.sp,
+            TextField(
+                value = searchQuery,
+                onValueChange = { searchQuery = it },
+                placeholder = { Text("Search for users...", color = Color.Gray) },
+                leadingIcon = {
+                    Icon(
+                        imageVector = ImageVector.vectorResource(id = R.drawable.search),
+                        contentDescription = "Search Icon",
+                        tint = MaterialTheme.colorScheme.surface
+                    )
+                },
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(start = 8.dp)
+                    .border(width = 0.dp, color = Color.Transparent),
+                singleLine = true,
+                colors = TextFieldDefaults.textFieldColors(
+                   containerColor = Color.Transparent
+                )
             )
+
+            Button(
+                onClick = { /* Handle search action */ },
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .padding(10.dp),
+                colors = ButtonDefaults.buttonColors(
+                     containerColor = ButtonColor,
+                     contentColor = Color.White,
+                ),
+                shape = RoundedCornerShape(4.dp)
+            ) {
+                Text(
+                    "Search",
+                    fontSize = 16.sp,
+                )
+            }
         }
     }
 }
--- app/src/main/java/com/cloudchef/greymobilegithubtask/presentation/repositories_list/SearchScreen.kt
+++ app/src/main/java/com/cloudchef/greymobilegithubtask/presentation/repositories_list/SearchScreen.kt
@@ -1,23 +1,27 @@
 package com.cloudchef.greymobilegithubtask.presentation.repositories_list
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.cloudchef.greymobilegithubtask.R
 import com.cloudchef.greymobilegithubtask.presentation.user_detail.GithubUserViewModel
 
 @Composable
@@ -48,5 +52,35 @@ fun SearchScreenView(modifier: Modifier, navController: NavController) {
         Spacer(modifier = Modifier.height(40.dp))
 
         SearchBar()
+
+        Spacer(modifier = Modifier.height(40.dp))
+
+        SearchEmptyStateView("Search Github for users")
+    }
+}
+
+@Composable
+fun SearchEmptyStateView(message: String) {
+    Box(modifier = Modifier
+        .fillMaxSize()) {
+        Column (
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center){
+
+            Icon(
+                painter = painterResource(id = R.drawable.search),
+                contentDescription = "No result found,",
+                modifier = Modifier
+                    .fillMaxHeight(0.4f)
+                    .fillMaxWidth())
+
+            Spacer(modifier = Modifier.height(20.dp))
+            Text(
+                text = message,
+                fontSize = 18.sp,
+                modifier = Modifier
+            )
+        }
     }
 }
